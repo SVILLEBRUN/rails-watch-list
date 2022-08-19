ff1ceac56c435161f5c5fd25f78d16f51043d92c
Sebastien VILLEBRUN <sebastien.villebrun@gmail.com>
2022-08-19 16:14:15 +0200

    UPGRADE List creation

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,7 +18,6 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    @list.save
     if @list.save
       redirect_to list_path(@list)
     else
@@ -29,6 +28,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
